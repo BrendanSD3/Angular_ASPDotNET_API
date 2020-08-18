460982394dc71b42aadbaf8e34c4c78a3e5134c0
bwall <brendan.wall03@gmail.com>
2020-08-18 12:07:55 +0100

    chapter 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,50 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EntriesComponent } from './entries/entries.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
+//services
+import {EntryService} from './entry.service';
+import { AppRouterModule } from './app-router.module';
+import { HttpClientModule } from '@angular/common/http';
+//Material Design
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule, MatInput} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {NewEntryComponent} from './new-entry/new-entry.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+//forms
+import {ReactiveFormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntriesComponent,
+    FooterComponent,
+    HeaderComponent,
+    NewEntryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    
+    //Material Design
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatInputModule,
+    MatCardModule,
+    MatSelectModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    AppRouterModule
   ],
-  providers: [],
+  providers: [EntryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/entry.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+@Injectable({
+  providedIn: 'root'
+})
+export class EntryService {
+
+  baseUrl:string='https://localhost:44387/api/entries/'
+  constructor(private http:HttpClient) { }
+
+  getAll(){
+      return this.http.get(this.baseUrl);
+  }
+  createEntry(entry){
+   return this.http.post(this.baseUrl, entry); 
+  }
+}
